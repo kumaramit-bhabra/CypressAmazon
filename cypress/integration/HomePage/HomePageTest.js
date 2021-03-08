6289c17b7b07ba62b9bdf4d5d0d64fef5dff5750
kumaramit-bhabra <amit14nov@yhaoo.com>
2021-03-08 16:57:36 +0000

    Changes to Home Page test

--- cypress/integration/HomePage/HomePageTest.js
+++ cypress/integration/HomePage/HomePageTest.js
@@ -4,20 +4,3 @@ import {Given, When, Then, And} from "cypress-cucumber-preprocessor/steps";
 import HomePage from "../../pageObjects/HomePage";
 
 const homePage = new HomePage();
-
-/*
-Given("User is on the Amazon homepage",function(){
-    homePage.isHomePageDisplayed(this.dataFixture);
-})
-
-When("Search is performed with keyword {string}",function($string){
-    homePage.enterSearchByKeyword($string);
-})
-
-Then("Search suggestion box result should have {string}",function($string){
-    homePage.isSearchSuggestionHavingKeyword($string);
-})
-
-And("I select one of the options and press Search button",function(){
-    homePage.selectOptionFromSuggestion();
-})*/
